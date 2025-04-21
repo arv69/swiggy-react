83e998c700a55316665635103b4c661d519995bb
geekykalpesh <geekykalpesh@gmail.com>
2025-04-22 04:30:28 +0530

    episode 5 completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import resList  from '../utils/mockData';
 const Body = () => {
+    const[listOfRestaurants, setListOfRestaurants] =useState(resList); 
+    const handleClickRating = () => {
+        const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4.5);
+        setListOfRestaurants(filteredList);
+    }  
+    const handleClickVeg = () => {
+        const filteredList = listOfRestaurants.filter((res) => res.info.veg === true);
+        setListOfRestaurants(filteredList);
+    }
+    const handleClickAll = () => {
+        setListOfRestaurants(resList);
+    }
+
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+            <button className="filter-btn" onClick={handleClickRating}>Top Rated Restaurants</button>
+            <button className="filter-btn" onClick={handleClickVeg} >Veg</button>
+            <button className='filter-btn' onClick={handleClickAll}>Reset</button>
+        </div>
         <div className="res-container">
-         {resList.map((resData) => {
-            return <RestaurantCard key={resData.info.id} resData={resData} />;
+         {listOfRestaurants.map((item) => {
+            return <RestaurantCard key={item.info.id} resData={item} />;
           }
           )}
         </div>
